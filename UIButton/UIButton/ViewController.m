660c693f3a2d404f4c4b31fafb0ce75dda4a70bf
周建宏 <stevenchou@zhoujianhongde-MacBook-Air.local>
2018-03-21 23:47:53 +0800

    no message

--- UIButton/UIButton/ViewController.m
+++ UIButton/UIButton/ViewController.m
@@ -17,6 +17,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(20, 30, 100, 200);
+    button.backgroundColor = [UIColor redColor];
+    [button setTitle:@"Click" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(changeColor) forControlEvents:(UIControlEventTouchUpInside)];
+    [self.view addSubview:button];
+    
+//    [button setBackgroundImage:[UIImage imageNamed:@"btn" ] forState:UIControlStateNormal];
+    
+    [button setImage:[UIImage imageNamed:@"btn" ] forState:UIControlStateNormal];
+}
+
+- (void)changeColor {
+    self.view.backgroundColor = [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1];
 }
 
 
